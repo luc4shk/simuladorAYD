1a73addf8a1f523cd63da2e8d18873fe0350ca6b
Luis Ascencio <luiscarlosasqu@ufps.edu.co>
2023-05-27 15:40:01 -0500

    Añadiendo rutas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,27 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import SideBar from './components/SideBar'
-import NavBar from './components/NavBar'
-import Page from './components/container/Page'
-import Login from './components/forms/Login'
+import React from 'react';
+import { useState } from 'react';
+import './App.css';
+import SideBar from './components/SideBar';
+import NavBar from './components/NavBar';
+import Page from './components/container/Page';
+import Login from './components/forms/Login';
+import { Router, Route, Switch } from 'wouter';
+import PrincipalPage from './pages/Admin/principal/PrincipalPage';
+import Competencias from './pages/Admin/competencias/Competencias';
+import {AppProvider} from './components/context/AppProvider';
+import AppRouter from './routes/AppRouter';
 
 function App() {
-  
-  const [open, setOpen] = useState(false)
-  const change = () => setOpen(!open)
+  // const [open, setOpen] = useState(false);
+  // const change = () => setOpen(!open);
+
   return (
-   <>
-   <Login/>
-   <SideBar isOpen={open}/>
-   <Page changeOpen={change} isOpen={open}/>
-   </> 
-  )
+    <AppProvider>
+      {/* <SideBar isOpen={open} /> */}
+      <SideBar/>
+      <AppRouter/>
+    </AppProvider>
+  );
 }
 
-export default App
+export default App;
