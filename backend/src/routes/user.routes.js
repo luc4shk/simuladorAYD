3287b756d1ee2e7a94be1a2d5cc75846d2d2e904
jenni-03 <jenniferdayanasr@ufps.edu.co>
2023-05-26 20:43:13 -0500

    Cambio en el modelo usuario

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,12 +1,32 @@
 const {Router} = require('express');
+const Usuario = require('../models/Usuario');
 
 // Inicializamos el router
 const router = Router();
 
-// Routes 
-router.get('/students', (req, res) => {});
+// Routes
+router.get('/students', async (req, res) => {
+    try {
+        const students = await Usuario.findAll({
+            where: {tipo: 'estudiante'}
+        })
+        res.json(students);
 
-router.get('/students/:id', (req, res) => {});
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+router.get('/students/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const students = await Usuario.findByPk(id);
+        res.json(students);
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
 
 // Importamos el router
 module.exports = router;
