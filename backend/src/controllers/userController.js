c40ff71a5bab140e61c1a23bf76da7648ac2feaf
jenni-03 <jenniferdayanasr@ufps.edu.co>
2023-05-30 14:56:49 -0500

    Controlador de usuario

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,29 +1,415 @@
 const Usuario = require('../models/Usuario');
+const multer = require('multer');
 
 getStudents =  async (req, res) => {
+
     try {
+
+        //Obtenemos los estudiantes
         const students = await Usuario.findAll({
             where: {tipo: 'estudiante'}
         })
-        res.json(students);
+
+        // Respondemos al usuario
+        res.status(200).json(students);
 
     } catch (error) {
         res.status(500).json({message: error.message});
     }
+    
 };
 
 getStudentById = async (req, res) => {
+    
     try {
-        const id = req.params.id;
-        const students = await Usuario.findByPk(id);
-        res.json(students);
+
+        //Obtenemos el id del estudiante
+        const {id} = req.params;
+
+        // Verificamos el id de entrada
+        const regexNum = /^[0-9]*$/; // Expresión regular que controla solo la admición de numeros
+
+        if(!regexNum.test(id)){
+            return res.status(400).json({error: 'id no valido'});
+        }
+
+        //Obtenemos el estudiante
+        const student = await Usuario.findOne({
+            where: {
+                id,
+                rol_id: 2
+            }
+        });
+
+        if(!student){
+            return res.status(400).json({error: 'No se encuentra ningun estudiante asociado con el id especificado'});
+        }
+
+        // Respondemos al usuario
+        res.status(200).json(student);
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+
+};
+
+updateStudentForS = async (req, res) => {
+
+    try {
+
+        //Obtenemos el id del estudiante a actualizar
+        const {id} = req.params;
+
+        // Verificamos el id de entrada
+        const regexNum = /^[0-9]*$/; // Expresión regular que controla solo la admición de numeros
+
+        if(!regexNum.test(id)){
+            return res.status(400).json({error: 'id no valido'});
+        }
+
+        //Obtenemos el estudiante
+        const student = await Usuario.findOne({
+            where: {
+                id,
+                rol_id: 2
+            }
+        });
+
+        if(!student){
+            return res.status(400).json({error: 'No se encuentra ningun estudiante asociado con el id especificado'});
+        }
+
+        // Obtenemos los datos a actualizar
+        const {nombre, apellido, password} = req.body;
+
+        const regexData = /^[a-zA-Z\s]*$/;
+
+        if(!regexData.test(nombre) || !regexData.test(apellido)){
+            return res.status(400).json({error: 'La sintaxis de los datos ingresados es incorrecta'});
+        }
+        
+        //Actualizamos el estudiante
+        student.update({
+            nombre,
+            apellido,
+            password
+        });
+
+        //Respondemos a la petición
+        res.status(200).json(student);
+
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+}
+
+//Métodos para el Director
+
+createStudent =  async (req, res) => {
+
+    try {
+
+        // Obtenemos los datos de el estudiante a crear
+        const {nombre, apellido, codigo, email, password, semestre} = req.body;
+
+        // Validamos los datos obtenidos
+        if(!nombre || !apellido || !codigo || !email || !password || !semestre){
+            return res.status(400).json({error: 'Todos los campos son requeridos'});
+        } 
+
+        const regexNum = /^[0-9]*$/;
+        const regexData = /^[a-zA-Z\s]*$/;
+
+        if(!regexData.test(nombre) || !regexData.test(apellido) || !regexNum.test(codigo) || !regexNum.test(semestre)){
+            return res.status(400).json({error: 'La sintaxis de los datos ingresados es incorrecta'});
+        }
+
+        //Validamos que el código sea único
+        const studentExist = await Usuario.findOne({
+            where: {
+                codigo
+            }
+        })
+
+        if(studentExist){
+            return res.status(400).json({error: 'El codigo del estudiante debe ser unico'});
+        }
+
+        const student = await Usuario.create({
+            nombre,
+            apellido,
+            codigo,
+            email,
+            password,
+            tipo: 'estudiante',
+            semestre,
+            rol_id: 2
+        })
+
+        // Respondemos al usuario
+        res.status(200).json(student);
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+
+};
+
+updateStudentForD = async (req, res) => {
+
+    try {
+
+        //Obtenemos el id del estudiante a actualizar
+        const {id} = req.params;
+
+        // Verificamos el id de entrada
+        const regexNum = /^[0-9]*$/; // Expresión regular que controla solo la admición de numeros
+
+        if(!regexNum.test(id)){
+            return res.status(400).json({error: 'id no valido'});
+        }
+
+        //Obtenemos el estudiante
+        const student = await Usuario.findByPk(id);
+
+        if(!student){
+            return res.status(400).json({error: 'No se encuentra ningun estudiante asociado con el id especificado'});
+        }
+
+        // Obtenemos los datos a actualizar
+        const {nombre, apellido, codigo, email, password, semestre, estado} = req.body;
+
+        const regexData = /^[a-zA-Z\s]*$/;
+
+        if(!regexData.test(nombre) || !regexData.test(apellido) || !regexNum.test(codigo) || !regexNum.test(semestre)){
+            return res.status(400).json({error: 'La sintaxis de los datos ingresados es incorrecta'});
+        }
+
+        //Comprobamos que no exista un estudiante con el mismo codigo
+        const studentExist = await Usuario.findOne({
+            where: {
+                codigo
+            }
+        });
+
+        if(studentExist){
+            res.status(400).json({error: "El codigo de el estudiante debe ser unico"});
+        }
+
+        //Actualizamos el estudiante
+        student.update({
+            nombre,
+            apellido,
+            codigo,
+            email,
+            password,
+            semestre,
+            estado
+        });
+
+        //Respondemos a la petición
+        res.status(200).json(student);
+
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+}
+
+getDirectors =  async (req, res) => {
+
+    try {
+
+        //Obtenemos los directores
+        const directors = await Usuario.findAll({
+            where: {tipo: 'director'}
+        })
+
+        // Respondemos al usuario
+        res.status(200).json(directors);
 
     } catch (error) {
         res.status(500).json({message: error.message});
     }
 };
 
+getDirectorById = async (req, res) => {
+
+    try {
+
+        //Obtenemos el id del director
+        const {id} = req.params;
+
+        // Verificamos el id de entrada
+        const regexNum = /^[0-9]*$/; // Expresión regular que controla solo la admición de numeros
+
+        if(!regexNum.test(id)){
+            return res.status(400).json({error: 'id no valido'});
+        }
+
+        //Obtenemos el director
+        const director = await Usuario.findOne({
+            where: {
+                id,
+                rol_id: 1
+            }
+        });
+
+        if(!director){
+            return res.status(400).json({error: 'No se encuentra ningun director asociado con el id especificado'});
+        }
+
+        // Respondemos al usuario
+        res.status(200).json(director);
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+};
+
+updateDirector = async (req, res) => {
+
+    try {
+
+        //Obtenemos el id del director a actualizar
+        const {id} = req.params;
+
+        // Verificamos el id de entrada
+        const regexNum = /^[0-9]*$/; // Expresión regular que controla solo la admición de numeros
+
+        if(!regexNum.test(id)){
+            return res.status(400).json({error: 'id no valido'});
+        }
+
+        //Obtenemos y verificamos el director
+        const director = await Usuario.findOne({
+            where: {
+                id,
+                rol_id: 1
+            }
+        });
+
+        if(!director){
+            return res.status(400).json({error: 'No se encuentra ningun director asociado con el id especificado'});
+        }
+
+        // Obtenemos los datos a actualizar
+        const {nombre, apellido, codigo, email, password, telefono, direccion, documento, celular, estado} = req.body;
+
+        const regexData = /^[a-zA-Z\s]*$/;
+
+        if(!regexData.test(nombre) || !regexData.test(apellido) || !regexNum.test(codigo) || !regexNum.test(telefono) || !regexNum.test(documento) || !regexNum.test(celular)){
+           return res.status(400).json({error: 'La sintaxis de los datos ingresados es incorrecta'});
+        }
+
+        //Comprobamos que no exista un director con el mismo codigo y documento
+        const directorExist = await Usuario.findOne({
+            where: {
+                codigo,
+                documento
+            }
+        });
+
+        if(directorExist && directorExist.id!=director.id){
+            res.status(400).json({error: "El codigo y documento de el director deben ser unicos"});
+        }
+
+        //Actualizamos el director
+        director.update({
+            nombre,
+            apellido,
+            codigo,
+            email,
+            password,
+            telefono,
+            direccion,
+            documento,
+            celular,
+            estado
+        });
+
+        //Respondemos a la petición
+        res.status(200).json(director);
+
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+}
+
+updatePhotoD = async (req, res) => {
+
+    try {
+
+        //Obtenemos el id del director a actualizar
+        const {id} = req.params;
+
+        // Verificamos el id de entrada
+        const regexNum = /^[0-9]*$/; // Expresión regular que controla solo la admición de numeros
+
+        if(!regexNum.test(id)){
+            return res.status(400).json({error: 'id no valido'});
+        }
+
+        //Obtenemos el director
+        const director = await Usuario.findByPk(id);
+
+        if(!director){
+            return res.status(400).json({error: 'No se encuentra ningun director asociado con el id especificado'});
+        }
+
+        //const file = req.files.archivo;
+        
+        // Configuración de almacenamiento de Multer
+        const storage = multer.diskStorage({
+
+            destination: (req, file, cb) => {
+                const ruta = './backend/src/multimedia/directors';
+                cb(null, ruta);
+            },
+            filename: (req, file, cb) => {
+                const ext = file.originalname.split('.').pop();
+                const nombreArchivo = `${director.documento}${ext}`;
+                cb(null, nombreArchivo);
+            },
+            
+        });
+
+        // Configuración de carga de Multer
+        const upload = multer({ storage }).single('file');
+    
+        // Ejecutamos la carga del archivo
+        upload(req, res, async (err) => {
+            if (err) {
+                return res.status(400).json(err.message);
+            }
+    
+            // Obtenemos la ruta de la foto
+            const ruta = `./backend/src/multimedia/directors/${director.documento}.${ext}`;
+
+            // Actualizamos el director
+            director.update({
+                foto_perfil: ruta
+            });
+    
+            // Respondemos a la petición
+            res.status(200).json(director);
+        });
+
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+}
+
+
+
 module.exports = {
+    createStudent,
     getStudents,
-    getStudentById
+    getStudentById,
+    updateStudentForS,
+    updateStudentForD,
+    getDirectors,
+    getDirectorById,
+    updateDirector,
+    updatePhotoD
 }
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -3,16 +3,31 @@ const {Router} = require('express');
 // Middleware de verificación de token
 const verifyJWT = require('../middlewares/verifyJWT');
 
-// Importamos las funciones del controladro
+// Importamos las funciones del controlador
 const userContoller = require('../controllers/userController');
+const fileUpload = require('express-fileupload');
 
 // Inicializamos el router
 const router = Router();
 
 // Routes
+router.post('/createS', verifyJWT, userContoller.createStudent);
+
 router.get('/students', verifyJWT, userContoller.getStudents);
 
-router.get('/students/:id', verifyJWT, userContoller.getStudentById);
+router.get('/student/:id', verifyJWT, userContoller.getStudentById);
+
+router.put('/updateStudentS/:id', verifyJWT, userContoller.updateStudentForS);
+
+router.put('/updateStudentD/:id', verifyJWT, userContoller.updateStudentForD);
+
+router.get('/directors', verifyJWT, userContoller.getDirectors);
+
+router.get('/director/:id', verifyJWT, userContoller.getDirectorById);
+
+router.put('/updateDirector/:id', verifyJWT, userContoller.updateDirector);
+
+router.put('/updatePhotoD/:id', fileUpload(), userContoller.updatePhotoD);
 
 // Importamos el router
 module.exports = router;
