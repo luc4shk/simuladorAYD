440ba7e8fb75a0d4276dabf5eb49e8b3901cb6f3
samuelboada21 <samuelomarboba@ufps.edu.co>
2023-06-06 14:02:13 -0500

    Formulario cambiar contraseña

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -13,8 +13,8 @@ import AgregarCompetencia from "../pages/Admin/competencias/AgregarCompetencia"
 import AgregarCategoria from '../pages/Admin/categorias/AgregarCategoria'
 import SeleccionarTipoPregunta from '../pages/Admin/preguntas/SeleccionarTipoPregunta'
 import PreguntaSimple from '../pages/Admin/preguntas/PreguntaSimple'
-import FormularioPrueba from '../components/forms/FormularioPrueba'
 import AgregarPrueba from '../pages/Admin/pruebas/AgregarPrueba'
+import CambiarContrasenia from '../pages/Admin/principal/CambiarContrasenia'
 export default function AppRouter() {
   return (
     <Router>
@@ -32,6 +32,7 @@ export default function AppRouter() {
           <Route path="/tipoPregunta" component={SeleccionarTipoPregunta}/>
           <Route path="/formularioPreguntaSimple" component={PreguntaSimple}/>
           <Route path="/crearPrueba" component={AgregarPrueba}/>
+          <Route path="/cambiarContrasenia" component={CambiarContrasenia}/>
           <Route component={ErrorPage}/>
         </Switch>
       </Router>
